26d6355613429fa582d917b34defe6deab71f1ce
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2022-10-09 16:16:08 -0500

    Reupload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Loader from './components/Loader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
